9d49e333f52731cbf07b30d7bace529f9fda37e5
Joris <contact@jorisreynes.fr>
2022-06-06 16:26:42 +0200

    Ajoutez des fichiers projet.

---
+++ generateur_mdp/outils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue, "ERREUR: Le nombre doit être positif et non nul");
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPresonnalise = null)
+        {
+
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                return nombre;
+            }
+            if (messageErreurPresonnalise == null)
+            {
+                Console.WriteLine("ERREUR : Le nombre doit être compris entre " + min + " et " + max);
+            }
+            else
+            {
+                Console.WriteLine(messageErreurPresonnalise);
+            }
+
+            Console.WriteLine();
+
+            return DemanderNombreEntre(question, min, max);
+        }
+
+        public static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Vous devez rentrer un nombre");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+}
